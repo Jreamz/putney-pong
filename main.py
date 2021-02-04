e8dfacaf0b6c1e45c457b67fdf35fd0b79862312
Jreamz <38327173+Jreamz@users.noreply.github.com>
2021-02-04 07:11:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+from score import Score
+import time
 
 screen = Screen()
 screen.title("Putney Pong")
 screen.bgcolor("black")
-screen.setup(800, 600)
+screen.setup(width=800, height=600)
 screen.tracer(0)
 
+score = Score()
 r_paddle = Paddle(350, 0)
 l_paddle = Paddle(-350, 0)
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.up, "Up")
@@ -19,11 +24,24 @@ screen.onkey(l_paddle.down, "s")
 
 game_over = False
 while not game_over:
+    time.sleep(ball.ball_speed)
     screen.update()
+    ball.move()
 
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
+    if ball.distance(r_paddle) < 50 and ball.xcor() >= 323 or ball.distance(l_paddle) < 50 and ball.xcor() <= -323:
+        ball.bounce_x()
 
+    if ball.xcor() > 380:
+        score.l_point()
+        score.update_score()
+        ball.ball_reset()
 
-
+    if ball.xcor() < -380:
+        score.r_point()
+        score.update_score()
+        ball.ball_reset()
 
 screen.exitonclick()
